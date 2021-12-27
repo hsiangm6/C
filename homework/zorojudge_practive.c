54ba90f9fe5d49efdf93ccea57c3edeee6817ce6
elaine <hsiangm6@gmail.com>
2021-12-28 00:52:43 +0800

    上傳檔案

--- homework/zorojudge_practive.c
+++ homework/zorojudge_practive.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
+float power(float base,int expo){ //次方運算函式
+    if(expo==0)
+    return 1;
+    else
+    return base*power(base,expo-1); //輸出值不斷乘base，直到expo=0，回傳次方後結果
+}
 int main(void){
-    int t,a[5];
-    scanf("%d",&t);
-    for(int i=0;i<t;i++){
-        scanf("%d %d %d %d",&a[0],&a[1],&a[2],&a[3]);
-        if(a[1]-a[0]==a[2]-a[1]){
-            a[4]=a[3]+(a[1]-a[0]);
-            for(int j=0;j<5;j++){
-                printf("%d ",a[j]);
-            }
-            printf("\n");
-        }
-        else{
-            a[4]=a[3]*(a[1]/a[0]);
-            for(int j=0;j<5;j++){
-                printf("%d ",a[j]);
-            }
-            printf("\n");
-        }
-    }
+    float base; //base=base
+    int expo; //expo=exponent
+    printf("Enter two numbers: "); //輸入base、expo
+    scanf("%f %d",&base,&expo);
+    printf("power(%.2f, %d)= %.2f",base,expo,power(base,expo)); //輸出次方後結果
     return 0;
 }
