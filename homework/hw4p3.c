a597d458e1d776e69c86897bd777370f71c1c3cb
hsiangm6桌機 <hsiangm6@gmail.com>
2022-01-11 00:09:34 +0800

    2022/1/11桌電

--- homework/hw4p3.c
+++ homework/hw4p3.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 void main(void){
-    int row1=0, column1=0, row2=0, column2=0;
-    printf("Enter row and column for the first matrix A: ");
+    int row1=0, column1=0, row2=0, column2=0; //row1=A矩陣的row; column1=A矩陣的column; row2=B矩陣的row; column2=B矩陣的column;
+    printf("Enter row and column for the first matrix A: "); //輸入A矩陣的row數、column數
     scanf("%d %d",&row1,&column1);
-    printf("\nEnter row and column for the first matrix B: ");
+    printf("\nEnter row and column for the first matrix B: "); //輸入B矩陣的row數、column數
     scanf("%d %d",&row2,&column2);
 
-    int A[5][5]={0}, B[5][5]={0}, matrix[5][5]={0};
-    printf("\nEnter elements of the first matrix:\n");
-    for(int i=0; i<row1; i++){
+    int A[5][5]={0}, B[5][5]={0}, matrix[5][5]={0}; //A=A矩陣; B=B矩陣; matrix=矩陣相乘的結果
+    printf("\nEnter elements of the first matrix:\n"); //input A matrix
+    for(int i=0; i<row1; i++){ 
         printf("Enter row %d:  ",i+1);
         for(int j=0; j<column1; j++){
             scanf("%d",&A[i][j]);
         }
     }
-    printf("\nEnter elements of the second matrix:\n");
+    printf("\nEnter elements of the second matrix:\n"); //input B matrix
     for(int i=0; i<row2; i++){
         printf("Enter row %d:  ",i+1);
         for(int j=0; j<column2; j++){
@@ -22,11 +22,13 @@ void main(void){
         }
     }
 
-    printf("The resultant matrix of A*B:\n");
+    printf("The resultant matrix of A*B:\n"); //output matrix multiplication result
     for(int i=0; i<row1; i++){
         for(int j=0; j<column2; j++){
-            matrix[i][j]=A[i][j]*B[j][i];
-            printf("%-2d ",matrix[i][j]);
+            for(int k=0; k<column1; k++){
+                matrix[i][j]+=A[i][k]*B[k][j]; //calculate result within the matrix
+            }
+            printf("%2d ",matrix[i][j]); //print the matrix
         }
         printf("\n");
     }
